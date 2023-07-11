48f87cb81fd4eea85a90ce57a815c7a263344fff
Zexuan <lzx213@sina.com>
2023-07-12 00:28:49 +0800

    Four steps completed, but not the think function

--- src/mcts_vanilla.py
+++ src/mcts_vanilla.py
@@ -6,6 +6,10 @@ from math import sqrt, log
 num_nodes = 1000
 explore_faction = 2.
 
+def UCT(node):
+    UCTvalue =  node.wins/node.visits + explore_faction * (sqrt(log(node.parent.visits) / node.visits))
+    return UCTvalue
+
 def traverse_nodes(node, board, state, identity):
     """ Traverses the tree until the end criterion are met.
 
@@ -18,9 +22,34 @@ def traverse_nodes(node, board, state, identity):
     Returns:        A node from which the next stage of the search can proceed.
 
     """
-    pass
+    #paaa
     # Hint: return leaf_node
 
+    currentNode = node
+    bestUCT = -999999
+    bestNode = None
+    #If the current node still has actions that have not been performed, jump out of the loop
+    while len(currentNode.untried_actions) == 0:
+        #Iterate over all child nodes
+        for childNode in currentNode.child_nodes.values():
+            #If the child node is not visited, meaning that the divisor is 0,uct is infinite, return it directly
+            if(childNode.visits == 0):
+                return childNode
+            
+            #Calculate uct
+            childUCT = UCT(childNode)
+            
+            #Update the current best node and uct
+            if childUCT > bestUCT:
+                bestUCT = childUCT
+                bestNode = childNode
+        ## Set the current best node as the starting point for the next round of the loop
+        currentNode = bestNode
+
+    return currentNode      
+    
+
+        
 
 def expand_leaf(node, board, state):
     """ Adds a new leaf to the tree by creating a new child node for the given node.
@@ -33,9 +62,27 @@ def expand_leaf(node, board, state):
     Returns:    The added child node.
 
     """
-    pass
+    #pass
     # Hint: return new_node
 
+    #Expand only if child nodes have been visited
+    if node.visits != 0:
+        #The action taken from the parent node that transitions the state to this node.
+        parentAction = node.untried_actions.pop()
+        #New state after action
+        nextState = board.next_state(state, parentAction)
+        #List of actions for a new node
+        actionList = board.legal_actions(nextState)
+
+        #Creating a new node
+        newNode = MCTSNode(node, parentAction, actionList)
+        #set as a child node
+        node.child_nodes[parentAction] = newNode
+
+        return newNode
+    
+    return node
+    
 
 def rollout(board, state):
     """ Given the state of the game, the rollout plays out the remainder randomly.
@@ -45,7 +92,18 @@ def rollout(board, state):
         state:  The state of the game.
 
     """
-    pass
+    #pass
+
+    #If the current state of the game is not over
+    while board.is_ended(state) == False:
+        #Perform a random act
+        randomAction = choice(board.legal_actions(state))
+        #Update state
+        state = board.next_state(state, randomAction)
+    
+    #
+    return board.points_values(state)
+
 
 
 def backpropagate(node, won):
@@ -56,8 +114,15 @@ def backpropagate(node, won):
         won:    An indicator of whether the bot won or lost the game.
 
     """
-    pass
+    #pass
 
+    #Update the number of visits and wins for the current node
+    node.visits += 1
+    node.wins += won
+
+    #Recursively update the parent node if it exists
+    if node.parent:
+        backpropagate(node.parent, node.parent.wins)
 
 def think(board, state):
     """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
@@ -80,7 +145,14 @@ def think(board, state):
         node = root_node
 
         # Do MCTS - This is all you!
+        leaf_node = traverse_nodes(node, board, sampled_game, identity_of_bot)
 
+        #It's not done yet.
+        #https://www.youtube.com/watch?v=UXW2yZndl7U 
+        #The current implementation is based on this video
+            
     # Return an action, typically the most frequently used action (from the root) or the action with the best
     # estimated win rate.
+    
+    
     return None
