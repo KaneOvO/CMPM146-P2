4e4064c06376253a931652f540f4159738193098
Zexuan <lzx213@sina.com>
2023-07-12 12:44:05 +0800

    It works but there are still bugs in the traverse

--- src/mcts_vanilla.py
+++ src/mcts_vanilla.py
@@ -6,9 +6,13 @@ from math import sqrt, log
 num_nodes = 1000
 explore_faction = 2.
 
-def UCT(node):
-    UCTvalue =  node.wins/node.visits + explore_faction * (sqrt(log(node.parent.visits) / node.visits))
-    return UCTvalue
+def UCT(node,identityJudge):
+    if identityJudge:
+        UCTvalue =  node.wins / node.visits + explore_faction * (sqrt(log(node.parent.visits) / node.visits))
+        return UCTvalue
+    else:
+        UCTvalue =  (1 - node.wins / node.visits) + explore_faction * (sqrt(log(node.parent.visits) / node.visits))
+        return UCTvalue
 
 def traverse_nodes(node, board, state, identity):
     """ Traverses the tree until the end criterion are met.
@@ -22,32 +26,39 @@ def traverse_nodes(node, board, state, identity):
     Returns:        A node from which the next stage of the search can proceed.
 
     """
-    #paaa
+    #pass
     # Hint: return leaf_node
 
     currentNode = node
     bestUCT = -999999
     bestNode = None
-    #If the current node still has actions that have not been performed, jump out of the loop
-    while len(currentNode.untried_actions) == 0:
+    bestAction = None
+    newState = state
+    identityJudge = identity == board.current_player(state)
+
+    #while the current node still has actions that have not been performed, jump out of the loop
+    while len(currentNode.untried_actions) < 1 and board.is_ended(state) == False:
         #Iterate over all child nodes
-        for childNode in currentNode.child_nodes.values():
-            #If the child node is not visited, meaning that the divisor is 0,uct is infinite, return it directly
-            if(childNode.visits == 0):
-                return childNode
-            
-            #Calculate uct
-            childUCT = UCT(childNode)
+        for child in currentNode.child_nodes.keys():
+            if(currentNode.child_nodes[child].visits == 0):
+                return currentNode.child_nodes[child], board.next_state(state, child)
+
+            childUCT = UCT(currentNode.child_nodes[child], identityJudge)
             
             #Update the current best node and uct
             if childUCT > bestUCT:
                 bestUCT = childUCT
-                bestNode = childNode
-        ## Set the current best node as the starting point for the next round of the loop
-        currentNode = bestNode
+                bestAction = child
+                bestNode = currentNode.child_nodes[child]
 
-    return currentNode      
-    
+        # Set the current best node as the starting point for the next round of the loop
+        currentNode = bestNode
+        newState = board.next_state(state, bestAction)
+
+
+    return currentNode, newState 
+
+
 
         
 
@@ -66,7 +77,7 @@ def expand_leaf(node, board, state):
     # Hint: return new_node
 
     #Expand only if child nodes have been visited
-    if node.visits != 0:
+    if len(node.untried_actions) > 0:
         #The action taken from the parent node that transitions the state to this node.
         parentAction = node.untried_actions.pop()
         #New state after action
@@ -79,10 +90,10 @@ def expand_leaf(node, board, state):
         #set as a child node
         node.child_nodes[parentAction] = newNode
 
-        return newNode
+        return newNode, nextState
     
-    return node
-    
+    return node, state
+
 
 def rollout(board, state):
     """ Given the state of the game, the rollout plays out the remainder randomly.
@@ -101,8 +112,8 @@ def rollout(board, state):
         #Update state
         state = board.next_state(state, randomAction)
     
-    #
-    return board.points_values(state)
+    
+    return state
 
 
 
@@ -122,7 +133,7 @@ def backpropagate(node, won):
 
     #Recursively update the parent node if it exists
     if node.parent:
-        backpropagate(node.parent, node.parent.wins)
+        backpropagate(node.parent, won)
 
 def think(board, state):
     """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
@@ -145,14 +156,32 @@ def think(board, state):
         node = root_node
 
         # Do MCTS - This is all you!
-        leaf_node = traverse_nodes(node, board, sampled_game, identity_of_bot)
+        leafNode, newState = traverse_nodes(node, board, sampled_game, identity_of_bot)
 
+        newLeaf, newState = expand_leaf(leafNode, board, newState)
+
+        newState = rollout(board, newState)
+
+        winValue = board.points_values(newState)
+
+        backpropagate(newLeaf, winValue[identity_of_bot])
+        
         #It's not done yet.
         #https://www.youtube.com/watch?v=UXW2yZndl7U 
         #The current implementation is based on this video
             
     # Return an action, typically the most frequently used action (from the root) or the action with the best
     # estimated win rate.
+
+    action = None
+    bestRate = -999999
+    for child in node.child_nodes.keys():
+        currRate = node.child_nodes[child].wins / node.child_nodes[child].visits
+        
+        if currRate > bestRate:
+            
+            bestRate = currRate
+            action = child
     
     
-    return None
+    return action
